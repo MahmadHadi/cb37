1d2db32eab17171b39eeea101e32c9ee7fae015d
MahmadHadi <nayanihadi97@gmail.com>
2024-09-04 09:34:00 +0530

    4 seo

---
+++ Array/basic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+
+    int arr[5];
+    int size = sizeof(arr) / sizeof(arr[0]); // ! size -> total number of elems in arr 
+    arr[0] = 10;
+    arr[1] = 11;
+    arr[2] = 13;
+    arr[3] = 14;
+    arr[4] = 15;
+
+    // * java -> index out of bound -> total elem = 10 -> 0 -> 9 -> arr[130] -> error -> index out of bound
+    // int i = 0;
+
+    // printf("arr[0] = %d \n", arr[i]);
+
+    // i++; // i = 1 
+    // printf("arr[0] = %d \n", arr[i]); // arr[1]
+
+    // i++; // i = 2 
+    // printf("arr[0] = %d \n", arr[i]); // arr[2]
+    // printf("arr[0] = %d \n", arr[3]);
+    // printf("arr[0] = %d \n", arr[4]);
+
+    for (int i = 0; i < size; i++) {
+        printf("arr[%d] = %d \n", i, arr[i]);
+    }
+    return 0;
+}
---
+++ basic/sizeof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int num = 1201;
+
+    printf("num = %d \n", num);
+    printf("sizeof(num) = %d \n", sizeof(num)); // 4 
+
+    char ch = 'a';
+    printf("sizeof(ch) = %d \n", sizeof(ch)); // 1
+    printf("sizeof(float) = %d \n", sizeof(float)); // 4 
+    printf("sizeof(bool) = %d \n", sizeof(bool)); // 1 
+    printf("sizeof(101) = %d \n", sizeof(101)); // 4 
+
+    int arr[100];
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("sizeof(arr) = %d \n", sizeof(arr));
+    printf("total num of elem in arr = %d \n", size);
+    return 0;
+}
